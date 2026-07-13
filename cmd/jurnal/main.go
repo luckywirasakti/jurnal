24b39c3bc6ec3fa5839c0c578796ef9ee078af1e
Lucky Wirasakti <lucky.wirasakti@gmail.com>
2026-07-13 15:04:29 +0000

    chore: bump version to v1.0.0 in main entrypoint

--- cmd/jurnal/main.go
+++ cmd/jurnal/main.go
@@ -8,7 +8,7 @@ import (
 	"github.com/luckywirasakti/jurnal/internal/cli"
 )
 
-const version = "0.3.0"
+const version = "1.0.0"
 
 func main() {
 	versionFlag := flag.Bool("v", false, "Show version and credits")
